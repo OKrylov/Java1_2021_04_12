e610adf12b542806b37a3661899acf8ad82730c6
OKrylov <krylov92oa@gmail.com>
2021-04-23 20:52:25 +0300

    Lesson 3 - homework

---
+++ src/ru/geekbrains/java1/lesson3/homework/Task3.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.java1.lesson3.homework;
+
+import java.util.Arrays;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            if (value < 6) {
+                array[i] = value * 2;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
